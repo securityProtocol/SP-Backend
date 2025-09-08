b4d721459fc2b2b4a6a7bfe083e83ebb5ade8d5f
kon28289 <kon28289@naver.com>
2025-09-08 11:33:30 +0900

    first commit

---
+++ src/main/java/com/sp/securityprotocol/SecurityProtocolApplication.java
@@ -0,0 +1,13 @@
+package com.sp.securityprotocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityProtocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sp/securityprotocol/SecurityProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.securityprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProtocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
